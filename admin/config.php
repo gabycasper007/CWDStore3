5f7e2164f59f6cff7e565d900d32a0239803e734
Gabriel <gabriell.vasile@gmail.com>
2013-10-10 15:43:34 +0300

    Configs and DB

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,5 @@
 <?php
-// HTTP
+/*// HTTP
 define('HTTP_SERVER', 'http://creativewebdesign.ro/cwdstore/admin/');
 define('HTTP_CATALOG', 'http://creativewebdesign.ro/cwdstore/');
 
@@ -25,5 +25,33 @@ define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'creativ2_cwdUser');
 define('DB_PASSWORD', 'cwdWebsiteUser13');
 define('DB_DATABASE', 'creativ2_cwdStore');
+define('DB_PREFIX', 'sy_');*/
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost/CWDStore3/admin/');
+define('HTTP_CATALOG', 'http://localhost/CWDStore3/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/CWDStore3/admin/');
+define('HTTPS_CATALOG', 'http://localhost/CWDStore3/');
+
+// DIR
+define('DIR_APPLICATION', 'I:/www/wamp/www/CWDStore3/admin/');
+define('DIR_SYSTEM', 'I:/www/wamp/www/CWDStore3/system/');
+define('DIR_DATABASE', 'I:/www/wamp/www/CWDStore3/system/database/');
+define('DIR_LANGUAGE', 'I:/www/wamp/www/CWDStore3/admin/language/');
+define('DIR_TEMPLATE', 'I:/www/wamp/www/CWDStore3/admin/view/template/');
+define('DIR_CONFIG', 'I:/www/wamp/www/CWDStore3/system/config/');
+define('DIR_IMAGE', 'I:/www/wamp/www/CWDStore3/image/');
+define('DIR_CACHE', 'I:/www/wamp/www/CWDStore3/system/cache/');
+define('DIR_DOWNLOAD', 'I:/www/wamp/www/CWDStore3/download/');
+define('DIR_LOGS', 'I:/www/wamp/www/CWDStore3/system/logs/');
+define('DIR_CATALOG', 'I:/www/wamp/www/CWDStore3/catalog/');
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cwdstore3');
 define('DB_PREFIX', 'sy_');
 ?>
--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-// HTTP
+/*// HTTP
 define('HTTP_SERVER', 'http://creativewebdesign.ro/cwdstore/');
 
 // HTTPS
@@ -27,5 +27,31 @@ define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'creativ2_cwdUser');
 define('DB_PASSWORD', 'cwdWebsiteUser13');
 define('DB_DATABASE', 'creativ2_cwdStore');
+define('DB_PREFIX', 'sy_');*/
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost/CWDStore3/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/CWDStore3/');
+
+// DIR
+define('DIR_APPLICATION', 'I:\www\wamp\www\CWDStore3/catalog/');
+define('DIR_SYSTEM', 'I:\www\wamp\www\CWDStore3/system/');
+define('DIR_DATABASE', 'I:\www\wamp\www\CWDStore3/system/database/');
+define('DIR_LANGUAGE', 'I:\www\wamp\www\CWDStore3/catalog/language/');
+define('DIR_TEMPLATE', 'I:\www\wamp\www\CWDStore3/catalog/view/theme/');
+define('DIR_CONFIG', 'I:\www\wamp\www\CWDStore3/system/config/');
+define('DIR_IMAGE', 'I:\www\wamp\www\CWDStore3/image/');
+define('DIR_CACHE', 'I:\www\wamp\www\CWDStore3/system/cache/');
+define('DIR_DOWNLOAD', 'I:\www\wamp\www\CWDStore3/download/');
+define('DIR_LOGS', 'I:\www\wamp\www\CWDStore3/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cwdstore3');
 define('DB_PREFIX', 'sy_');
 ?>
